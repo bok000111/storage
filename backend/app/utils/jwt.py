fb9545b1c4699ad8cdb7ccca8f1d354908a5d963
jbok <junseok@jbok.info>
2024-12-14 16:23:12 +0900

    impl refresh token

--- backend/app/utils/jwt.py
+++ backend/app/utils/jwt.py
@@ -1,9 +1,13 @@
 from datetime import datetime, timedelta, timezone
-from typing import Optional
-import jwt
+
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
 from passlib.context import CryptContext
+import jwt
+
 from app.config import settings
+from app.models.auth import RefreshTokenModel
+from app.schemas.auth import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -17,11 +21,23 @@ def verify_password(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(
+    data: dict, expires_delta=timedelta(minutes=settings.jwt_exp_min)
+):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (
-        expires_delta or timedelta(minutes=settings.jwt_exp_min)
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.jwt_secret, algorithm=settings.jwt_algorithm
     )
+    return encoded_jwt
+
+
+def create_refresh_token(
+    data: dict, expires_delta=timedelta(days=settings.jwt_exp_day)
+):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         to_encode, settings.jwt_secret, algorithm=settings.jwt_algorithm
@@ -39,3 +55,14 @@ def decode_token(token: str):
         raise jwt.ExpiredSignatureError
     except jwt.PyJWTError:
         raise jwt.InvalidTokenError
+
+
+async def register_refresh_token(user: User, token: str, db: AsyncSession):
+    refresh_token = RefreshTokenModel(
+        token=token,
+        user_id=user.id,
+        expires_at=datetime.now(timezone.utc) + timedelta(days=settings.jwt_exp_day),
+    )
+
+    db.add(refresh_token)
+    await db.commit()
