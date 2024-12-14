52d35b3923567b9f8e31907253937a503c04d477
jbok <junseok@jbok.info>
2024-12-14 15:15:16 +0900

    refactor authentication flow to handle token payload and improve user retrieval

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -1,22 +1,40 @@
 from typing import Annotated, Optional
 
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
 from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 
 from app.database import get_db
 from app.models.auth import UserModel
+from app.schemas.auth import User
 from app.utils.jwt import decode_token, verify_password, oauth2_scheme
+
+
+async def get_token_payload(token: Annotated[str, Depends(oauth2_scheme)]):
+    if not token:
+        return None
+    try:
+        payload = decode_token(token)
+        if not payload:
+            return None
+    except ExpiredSignatureError:
+        return None
+    except InvalidTokenError:
+        return None
+    return payload.get("sub")
 
 
 async def get_user(
     id: Optional[int] = None,
     username: Optional[str] = None,
     email: Optional[str] = None,
+    payload: Optional[str] = Depends(get_token_payload),
     db: AsyncSession = Depends(get_db),
 ):
-    if not any([id, username, email]):
+    if not any([id, username, email, payload]):
         return None
 
     query = select(UserModel)
@@ -26,50 +44,53 @@ async def get_user(
         query = query.where(UserModel.username == username)
     elif email:
         query = query.where(UserModel.email == email)
+    elif payload:
+        query = query.where(UserModel.username == payload)
 
     try:
         result = await db.execute(query)
-        return result.scalars().first()
-    except Exception as e:
-        print(f"Error fetching user: {e}")
-    return None
+        user_model = result.scalars().first()
+        if not user_model:
+            return None
+        return User.model_validate(user_model)
+    except SQLAlchemyError as orm_err:
+        print(f"Error fetching user: {orm_err}")
+        return None
 
 
 async def auth_user(
-    username: str, password: str, db: Annotated[AsyncSession, Depends(get_db)]
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[AsyncSession, Depends(get_db)],
 ):
-    result = await db.execute(select(UserModel).where(UserModel.username == username))
-    user = result.scalars().first()
-    if not user or not verify_password(password, user.password):
-        return None
-    return user
+    try:
+        result = await db.execute(
+            select(UserModel).where(UserModel.username == form_data.username)
+        )
+        user_model = result.scalars().first()
+        if not user_model or not verify_password(
+            form_data.password, user_model.password
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return User.model_validate(user_model)
+    except SQLAlchemyError as orm_err:
+        print(f"Error during authentication: {orm_err}")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
 async def get_current_user(
-    token: Annotated[str, Depends(oauth2_scheme)],
-    db: Annotated[AsyncSession, Depends(get_db)],
+    user: Annotated[User, Depends(get_user)],
 ):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid or expired credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = decode_token(token)
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except ExpiredSignatureError:
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
+            detail="Invalid credentials",
         )
-    except InvalidTokenError:
-        raise credentials_exception
-
-    user = await get_user(username=username, db=db)
-    if user is None:
-        raise credentials_exception
-
     return user
